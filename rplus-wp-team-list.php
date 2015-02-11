b865cabc2bb2e59d71cb459b4767185306cdf5b0
Pascal Birchler <hello@pascalbirchler.ch>
2015-02-11 12:48:54 +0100

    Remove non-existent activation hooks

--- rplus-wp-team-list.php
+++ rplus-wp-team-list.php
@@ -30,11 +30,6 @@ if ( ! defined( 'WPINC' ) ) {
 require_once( plugin_dir_path( __FILE__ ) . 'class-rplus-wp-team-list.php' );
 require_once( plugin_dir_path( __FILE__ ) . 'widgets/class-rplus-wp-team-list-widget.php' );
 
-// Register hooks that are fired when the plugin is activated or deactivated.
-// When the plugin is deleted, the uninstall.php file is loaded.
-register_activation_hook( __FILE__, array( 'WP_Team_List', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'WP_Team_List', 'deactivate' ) );
-
 // As soon as WP is ready we load the instance of the plugin
 add_action( 'plugins_loaded', array( 'WP_Team_List', 'get_instance' ) );
 
