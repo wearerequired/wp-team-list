86174c744dc35587f863e06598cfa1c0bdaccb66
Dominik Schilling <dominikschilling+git@gmail.com>
2017-01-18 22:30:33 +0100

    Define plugin_main_file for wp-deploy task.
    See #41.

--- grunt/wp_deploy.js
+++ grunt/wp_deploy.js
@@ -1,10 +1,11 @@
 module.exports = {
 	dist: {
 		options: {
-			plugin_slug: 'wp-team-list',
-			svn_user   : 'wearerequired',
-			build_dir  : 'release/svn/',
-			assets_dir : 'assets/'
+			plugin_slug:      'wp-team-list',
+			plugin_main_file: 'rplus-wp-team-list.php',
+			svn_user:         'wearerequired',
+			build_dir:        'release/svn/',
+			assets_dir:       'assets/'
 		}
 	}
 }
