707bc753458c05fd622101d97b4e1bf9c1d5e351
Dominik Schilling <dominikschilling+git@gmail.com>
2019-03-11 15:15:11 +0200

    Remove file header

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,6 @@
 <?php
 /**
  * Delete all user meta data when the plugin is uninstalled.
- *
- * @package   WP_Team_List
- * @author    Silvan Hagen <silvan@required.ch>
- * @license   GPL-2.0+
- * @link      https://github.com/wearerequired/wp-team-list/
- * @copyright 2015 required gmbh
  */
 
 // If uninstall, not called from WordPress, then exit.
